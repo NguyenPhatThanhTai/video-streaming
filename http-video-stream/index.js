a46d61fcb8305b1f5130d736c7916e5a435d08fe
NguyenPhatThanhTai <57514749+NguyenPhatThanhTai@users.noreply.github.com>
2021-09-23 17:47:59 +0700

    Update 1.2

--- http-video-stream/index.js
+++ http-video-stream/index.js
@@ -84,6 +84,6 @@ app.get("/video", function(req, res) {
     }
 });
 
-app.listen(8000, function() {
-    console.log("Listening on port 8000!");
+app.listen(process.env.PORT || 8000, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
