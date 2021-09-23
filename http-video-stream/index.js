e6bc4f2c92bffb14ce1f44889c6b2fda27fc6b6b
NguyenPhatThanhTai <57514749+NguyenPhatThanhTai@users.noreply.github.com>
2021-09-23 17:59:46 +0700

    Update 1.3

--- http-video-stream/index.js
+++ http-video-stream/index.js
@@ -8,41 +8,8 @@ app.get("/", function(req, res) {
 });
 
 app.get("/video", function(req, res) {
-    // // Ensure there is a range given for the video
-    // const range = req.headers.range;
-    // if (!range) {
-    //     res.status(400).send("Requires Range header");
-    // }
-
-    // // get video stats (about 61MB)
-    // const videoPath = "http://ia800300.us.archive.org/1/items/night_of_the_living_dead/night_of_the_living_dead_512kb.mp4";
-    // const videoSize = fs.statSync("http://ia800300.us.archive.org/1/items/night_of_the_living_dead/night_of_the_living_dead_512kb.mp4").size;
-
-    // // Parse Range
-    // // Example: "bytes=32324-"
-    // const CHUNK_SIZE = 10 ** 6; // 1MB
-    // const start = Number(range.replace(/\D/g, ""));
-    // const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-
-    // // Create headers
-    // const contentLength = end - start + 1;
-    // const headers = {
-    //     "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-    //     "Accept-Ranges": "bytes",
-    //     "Content-Length": contentLength,
-    //     "Content-Type": "video/mp4",
-    // };
-
-    // // HTTP Status 206 for Partial Content
-    // res.writeHead(206, headers);
-
-    // // create video read stream for this particular chunk
-    // const videoStream = fs.createReadStream(videoPath, { start, end });
-
-    // // Stream the video chunk to the client
-    // videoStream.pipe(res);
-
-    var fileUrl = 'https://ia601402.us.archive.org/35/items/KSCNMD-S2-09-720/Kobayashi-san%20Chi%20no%20Maid%20Dragon%20S2%20-%2009.720.ia.mp4';
+    // var fileUrl = 'https://ia601402.us.archive.org/35/items/KSCNMD-S2-09-720/Kobayashi-san%20Chi%20no%20Maid%20Dragon%20S2%20-%2009.720.ia.mp4';
+    var fileUrl = req.query.url;
 
     var range = req.headers.range;
     var positions, start, end, total, chunksize;
